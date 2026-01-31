7e17b16fb60ce190647a749ff2c279141194e084
itamarprincipios <itamarprincipios@gmail.com>
2026-01-31 02:17:21 -0400

    fix: adiciona headers para prevenir cache de equipes

--- professor/teams.php
+++ professor/teams.php
@@ -5,6 +5,11 @@ require_once '../includes/db.php';
 
 requireProfessor();
 
+// Prevent caching
+header("Cache-Control: no-cache, no-store, must-revalidate");
+header("Pragma: no-cache");
+header("Expires: 0");
+
 $pageTitle = 'Minhas Equipes';
 
 include '../includes/header.php';
