eb09a6fb755433772bf7976971f7c9a9a5479c13
itamarprincipios <itamarprincipios@gmail.com>
2026-02-07 18:23:18 -0400

    Cleanup diagnostic files

--- check_active_events.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once 'includes/db.php';
-$events = query("SELECT id, name, active_flag FROM competition_events WHERE active_flag = 1");
-header('Content-Type: application/json');
-echo json_encode($events, JSON_PRETTY_PRINT);
--- proxy_check.php
+++
@@ -1,5 +0,0 @@
-<?php
-$url = "https://sistemajem.online/api/standings-api.php?action=group_standings&event_id=3&modality_id=12&category_id=5&gender=F";
-$resp = file_get_contents($url);
-header('Content-Type: application/json');
-echo $resp;
